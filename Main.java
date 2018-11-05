8907f700a12569bcb11e7e298d56007aa7b9df13
Xuki Nuki <32760261+startars@users.noreply.github.com>
2018-11-05 12:57:55 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,58 @@
+
+import java.net.MalformedURLException;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        // Make sure that this directory exists
+        String dirName = "C:\\";
+        try {
+            saveFileFromUrlWithJavaIO(
+                    //on adroid chage fileURL to: /sdcard/download
+                    dirName + "\\file.txt", "https://www.wikipedia.org/");
+            System.out.println("finished");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Using Java IO
+    public static void saveFileFromUrlWithJavaIO(String fileName, String fileUrl)
+            throws MalformedURLException, IOException {
+        BufferedInputStream in = null;
+        FileOutputStream fout = null;
+        try {
+            in = new BufferedInputStream(new URL(fileUrl).openStream());
+            fout = new FileOutputStream(fileName);
+            byte data[] = new byte[1024];
+            int count;
+            while ((count = in.read(data, 0, 1024)) != -1) {
+                fout.write(data, 0, count);
+            }
+        } finally {
+            if (in != null)
+                in.close();
+            if (fout != null)
+                fout.close();
+        }
+    }
+
+    // Using Commons IO library
+    // Available at http://commons.apache.org/io/download_io.cgi
+    public static void saveFileFromUrlWithCommonsIO(String fileName,
+                                                    String fileUrl) throws MalformedURLException, IOException {
+        FileUtils.copyURLToFile(new URL(fileUrl), new File(fileName));
+    }
+}
